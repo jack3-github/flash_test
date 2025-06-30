3895e9d6bcb4d40bbd99765ec0b40baaa3207567
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 06:37:44 +0900

    change

--- data.js
+++ data.js
@@ -3,19 +3,19 @@
 
 const flashcardsData = [
     {
-        questionImage: './images/5+2.jpg', // テスト用の画像URL2
+        questionImage: './images/5plus2.jpg', // テスト用の画像URL2
         answerText: 'りんご',
     },
     {
-        questionImage: './images/5+2.jpg', // テスト用の画像URL3
+        questionImage: './images/5plus2.jpg', // テスト用の画像URL3
         answerText: '犬',
     },
     {
-        questionImage: './images/5+2.jpg', // テスト用の画像URL4
+        questionImage: './images/5plus2.jpg', // テスト用の画像URL4
         answerText: 'パソコン',
     },
     {
-        questionImage: './images/5+2.jpg', // テスト用の画像URL5
+        questionImage: './images/5plus2.jpg', // テスト用の画像URL5
         answerText: 'コーヒーカップ',
     },
     // 必要に応じて、さらに問題を追加してください。
