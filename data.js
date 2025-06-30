1a62abcf687e6dd16b538c554fe7f594974c0951
jack3 <41743337+jack3-github@users.noreply.github.com>
2025-07-01 06:28:35 +0900

    Update data.js

--- data.js
+++ data.js
@@ -7,15 +7,15 @@ const flashcardsData = [
         answerText: 'りんご',
     },
     {
-        questionImage: 'https://picsum.photos/300/202', // テスト用の画像URL3
+        questionImage: './images/5+2.jpg', // テスト用の画像URL3
         answerText: '犬',
     },
     {
-        questionImage: 'https://picsum.photos/300/203', // テスト用の画像URL4
+        questionImage: './images/5+2.jpg', // テスト用の画像URL4
         answerText: 'パソコン',
     },
     {
-        questionImage: 'https://picsum.photos/300/204', // テスト用の画像URL5
+        questionImage: './images/5+2.jpg', // テスト用の画像URL5
         answerText: 'コーヒーカップ',
     },
     // 必要に応じて、さらに問題を追加してください。
